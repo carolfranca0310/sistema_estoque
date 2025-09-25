253ab8d14d3566429c6badf688db9cb6c0f946dd
Carol Araujo <carolfranca0310@gmail.com>
2025-09-25 15:32:57 -0300

    test: add new service tests

---
+++ tests/InventoryManagement.Tests/Teste.Service/ProductsInfoServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using InventoryManagement.Domain.Interfaces.IRepository;
+using InventoryManagement.Service.Services;
+using Moq;
+
+namespace InventoryManagement.Tests.Teste.Service
+{
+    public class ProductsInfoServiceTests
+    {
+        private readonly Mock<IProductInfoRepository> _productInfoRepositoryMock;
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+        private readonly ProductInfoService _productInfoService;
+
+        public ProductsInfoServiceTests()
+        {
+            _productInfoRepositoryMock = new Mock<IProductInfoRepository>();
+            _productRepositoryMock = new Mock<IProductRepository>();
+
+            _productInfoService = new ProductInfoService(
+                _productInfoRepositoryMock.Object,
+                _productRepositoryMock.Object
+            );
+        }
+    }
+}
+
